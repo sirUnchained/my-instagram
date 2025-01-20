eacbf03494f70ff4a7b647f4c192428bb1ce0797
sirUnchained <bbehnam3831@gmail.com>
2025-01-20 10:32:11 +0330

    start adding chose follow/unfolllow feature

--- src/modules/profiles/profiles.routes.js
+++ src/modules/profiles/profiles.routes.js
@@ -14,10 +14,14 @@ router.route("/follow/:username").get(authorization, controller.follow);
 
 router.route("/unfollow/:username").get(authorization, controller.unfollow);
 
+// router.route("/accept-follower")
+
+// router.route("/reject-follower")
+
 router
-.route("/edit/:username")
-.post(authorization, uploader.single("profile"), controller.updateProfile)
-.get(authorization, controller.showEditProfile);
+  .route("/edit/:username")
+  .post(authorization, uploader.single("profile"), controller.updateProfile)
+  .get(authorization, controller.showEditProfile);
 
 router.route("/:username").get(authorization, controller.showProfileView);
 
